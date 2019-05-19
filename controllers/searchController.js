ef77b0dfdf32d45a497483b599f80f1f2cdd8ef1
Andrew Smith <andrew@boosmith.com>
2019-05-19 16:43:37 +0100

    Started on forming name value result

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,6 +4,7 @@ const readline = require("readline");
 const getSearchResults = async searchQuery => {
   try {
     if (searchQuery.length > 2) {
+      
       const searchResults = [];
       const fileStream = fs.createReadStream("data/namesList.csv");
       const rl = readline.createInterface({
@@ -13,7 +14,11 @@ const getSearchResults = async searchQuery => {
       for await (const line of rl) {
         if (line.toLowerCase().indexOf(searchQuery) > -1) {
           dataObj = line.split(',')
-          searchResults.push(dataObj);
+          const result = {};
+          for (let i = 0; i <= dataObj.length; i++) {
+            result[headers[i].value] = dataObj[i].value
+          }
+          searchResults.push(result);
         }
       }
       console.log(`Search results = ${searchResults}`);
