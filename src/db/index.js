3b917f936841d0d7e14732aa026f4fdfcd7972fc
Andrew Smith <boosmith@gmail.com>
2019-08-22 15:26:29 +0100

    refactor #9: changed back to original mongo uri code

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost:27017";
-const mongoDb = mongoUrl + "/trelloid";
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/trelloid";
 
 mongoose
   .set("useNewUrlParser", true)
   .set("useFindAndModify", false)
   .set("useCreateIndex", true)
-  .connect(mongoDb, err => {
+  .connect(url, err => {
     if (err) {
       console.log("Error in connection");
       throw err;
