bc20fe78979bbcad26ee66e798d2bd35492b9384
Andrew Smith <boosmith@gmail.com>
2019-08-26 23:09:18 +0100

    feature #11: remove jwt functionality

--- src/api/user/userModel.js
+++ src/api/user/userModel.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
-  address: { type: String, required: true },
-  city: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  password: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
   postcode: { type: String, required: true }
 });
 
@@ -21,30 +19,6 @@ userSchema.index({
 userSchema.virtual("name").get(() => {
   return this.lastName + ", " + this.firstName;
 });
-
-userSchema.pre("save", function(next) {
-  const user = this;
-  if (!this.isModified("password")) {
-    next();
-  } else {
-    bcrypt.hash(user.password, 10, function(err, hash) {
-      if (err) {
-        next(err);
-      } else {
-        user.password = hash;
-        next();
-      }
-    });
-  }
-});
-userSchema.methods = {
-  comparePassword(receivedPassword, next) {
-    bcrypt.compare(receivedPassword, this.password, function(err, isMatch) {
-      if (err) return next(err);
-      next(null, isMatch);
-    });
-  }
-};
 
 const userModel = mongoose.model("User", userSchema);
 
