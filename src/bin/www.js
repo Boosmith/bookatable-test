e7fa0b00a20dfb5e79c0f2b738c6a908efd4fbe6
Andrew Smith <boosmith@gmail.com>
2019-08-26 14:58:35 +0100

    chore

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const app = require("../app");
-const debug = require("debug");
 const http = require("http");
 
 const environment = process.env.NODE_ENV;
@@ -10,7 +9,6 @@ const config = require("../config");
 
 const stage = config[environment];
 const port = stage.port;
-debug("api-express-mongo:server");
 
 app.set("port", port);
 
@@ -18,7 +16,6 @@ const server = http.createServer(app);
 
 server.listen(port);
 server.on("error", onError);
-server.on("listening", onListening);
 
 function onError(error) {
   if (error.syscall !== "listen") {
@@ -38,10 +35,4 @@ function onError(error) {
     default:
       throw error;
   }
-}
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
 }
