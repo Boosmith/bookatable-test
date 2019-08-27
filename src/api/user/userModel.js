9fbddccefdd793a847dd30d238a1b617c3e68564
Andrew Smith <boosmith@gmail.com>
2019-08-27 17:30:39 +0100

    feature #11: User model password hashing.

--- src/api/user/userModel.js
+++ src/api/user/userModel.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
@@ -22,6 +23,30 @@ userSchema.index({
 userSchema.virtual("name").get(() => {
   return this.lastName + ", " + this.firstName;
 });
+
+userSchema.pre("save", function(next) {
+  const user = this;
+  if (!this.isModified("password")) {
+    next();
+  } else {
+    bcrypt.hash(user.password, 10, function(err, hash) {
+      if (err) {
+        next(err);
+      } else {
+        user.password = hash;
+        next();
+      }
+    });
+  }
+});
+userSchema.methods = {
+  comparePassword(receivedPassword, next) {
+    bcrypt.compare(receivedPassword, this.password, function(err, isMatch) {
+      if (err) return next(err);
+      next(null, isMatch);
+    });
+  }
+};
 
 const userModel = mongoose.model("User", userSchema);
 
