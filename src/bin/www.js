1fe9f7cecb36a636d3ae3500b21c230e78045d72
Andrew Smith <boosmith@gmail.com>
2020-02-03 13:35:08 +0000

    refactor: reverted to non Folktale version

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,10 +1,7 @@
-const Maybe = require('folktale/maybe');
+#!/usr/bin/env node
+
 const http = require('http');
 const app = require('../app');
-
-const { pid } = process;
-
-const { Just, Nothing } = Maybe;
 
 const environment = process.env.NODE_ENV;
 
@@ -16,44 +13,30 @@ const { port } = stage;
 app.set('port', port);
 
 const server = http.createServer(app);
-const mainIsModule = (module, main) => main === module;
 
-const startServerIfCommandline = (main, module) =>
-  mainIsModule(main, module)
-    ? Just(
-        server.listen(port, () => {
-          console.log(`Listening on port ${port}!`);
-          console.log(`Started process: ${pid}`);
-        })
-      )
-    : Nothing();
+server.listen(port, () => {
+  console.log(`Started process ${process.pid}`);
+  console.log(`Server listening on port ${port}`);
+});
 
-if (server)
-  server.on('error', error => {
-    if (error.syscall !== 'listen') {
+server.on('error', error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
       throw error;
-    }
-
-    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-
-    switch (error.code) {
-      case 'EACCES':
-        console.error(`${bind} requires elevated privileges`);
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(`${bind} is already in use`);
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  });
-
-function testMe() {
-  return true;
-}
-
-module.exports = { startServerIfCommandline, testMe };
-
-startServerIfCommandline(require.main, module, app, port);
+  }
+});
